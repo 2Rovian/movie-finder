eca733a7db6e92bb18925731ac57b37158c1a6d5
ian <iangurgel25@hotmail.com>
2025-02-13 16:54:35 -0300

    criado Top_Rated card

--- client-side/src/components/FetchMovies.jsx
+++ client-side/src/components/FetchMovies.jsx
@@ -2,19 +2,12 @@ import { useState } from 'react'
 import { useQuery } from 'react-query'
 import MovieCard from './MovieCard';
 import MostViewedCard from './MostViewedCard';
+import TopRatedCard from './TopRatedCard';
 
 function FetchMovies() {
 
     const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
     const BASE_URL = "https://api.themoviedb.org/3/"
-
-    // const API_OPTIONS = {
-    //     method: 'GET',
-    //     headers: {
-    //         accept: 'application/json',
-    //         Authorization: `Bearer ${API_KEY}`
-    //     }
-    // }
 
     const { data, isLoading, error } = useQuery('movies', () =>
         fetch(`${BASE_URL}movie/popular?api_key=${API_KEY}&page=1`)
@@ -34,7 +27,10 @@ function FetchMovies() {
                 ))}
             </div>
             
-            <MostViewedCard BASE_URL={ BASE_URL } API_KEY={ API_KEY }/>
+            <div className='w-[95%] mx-auto lg:max-w-[400px] lg:mt-5 lg:mr-3 flex flex-col gap-y-4'>
+                <MostViewedCard BASE_URL={ BASE_URL } API_KEY={ API_KEY }/>
+                <TopRatedCard BASE_URL={ BASE_URL } API_KEY={ API_KEY }/>
+            </div>
         </div>
     )
 }
--- client-side/src/components/TopRatedCard.jsx
+++ client-side/src/components/TopRatedCard.jsx
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { useQuery } from 'react-query';
+import React from 'react'
+import { useQuery } from 'react-query'
 
-function MostViewedCard({ BASE_URL, API_KEY }) {
-    const [DayBtn, setDayBtn] = useState(true);
-    const [WeekBtn, setWeekBtn] = useState(false);
-    const [MonthBtn, setMonthBtn] = useState(false);
 
-    const [time, setTime] = useState('day');
+function TopRatedCard({ BASE_URL, API_KEY }) {
 
-    const { data, isLoading, error } = useQuery(['trending_movies', time], () =>
-        fetch(`${BASE_URL}trending/movie/${time}?api_key=${API_KEY}`)
+    const { data, isLoading, error } = useQuery('Top_Rated', () =>
+        fetch(`${BASE_URL}movie/top_rated?api_key=${API_KEY}`)
             .then((res) => res.json())
     );
 
@@ -18,25 +14,14 @@ function MostViewedCard({ BASE_URL, API_KEY }) {
 
     return (
         <main className='w-full bg-slate-900 h-fit rounded-t-xl rounded-b-lg flex flex-col'>
-            <div className='py-5 lg:py-3 flex px-4 gap-x-4 items-center bg-slate-950 rounded-t-xl lg:flex-col'>
-                <span className='text-blue-400 font-bold text-xl lg:pb-3 lg:text-2xl lg:tracking-wide'>Most Viewed</span>
+            <div className='py-5 flex justify-center bg-slate-950 rounded-t-xl '>
+                <span className='text-blue-400 font-bold text-2xl tracking-wide'>Top Rated</span>
 
-                <div className='items-center flex h-full gap-x-4'>
-                    <button className={`border-2 rounded-xl px-4 py-1 cursor-pointer duration-500 ease-in-out ${DayBtn ? "border-blue-400 text-blue-400" : "border-blue-900 text-blue-900"}`}
-                        onClick={() => { setDayBtn(true); setWeekBtn(false); setMonthBtn(false); setTime('day') }}
-                    >Day</button>
-                    <button className={`border-2 rounded-xl px-4 py-1 cursor-pointer duration-500 ease-in-out ${WeekBtn ? "border-blue-400 text-blue-400" : "border-blue-900 text-blue-900"}`}
-                        onClick={() => { setDayBtn(false); setWeekBtn(true); setMonthBtn(false); setTime('week') }}
-                    >Week</button>
-                    <button className={`border-2 rounded-xl px-4 py-1 cursor-pointer duration-500 ease-in-out ${MonthBtn ? "border-blue-400 text-blue-400" : "border-blue-900 text-blue-900"}`}
-                        onClick={() => { setDayBtn(false); setWeekBtn(false); setMonthBtn(true); setTime('day') }}
-                    >Month</button>
-                </div>
             </div>
-
+            
             <div className='grow p-2'>
                 <div className='gap-y-2 flex flex-col'>
-                    {data?.results.slice(0, 10).map((movie) => (
+                    {data?.results.slice(0, 6).map((movie) => (
                         <div key={movie.id} className='bg-slate-800 rounded-lg flex p-2 gap-x-2 hover:outline-blue-400 duration-[400ms] hover:shadow hover:scale-105 ease-in-out cursor-pointer outline outline-transparent'>
                             <div>
                                 <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" className='object-cover rounded-md size-16' />
@@ -64,8 +49,7 @@ function MostViewedCard({ BASE_URL, API_KEY }) {
                 </div>
             </div>
         </main>
-
     )
 }
 
-export default MostViewedCard
+export default TopRatedCard
