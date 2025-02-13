7ed07eff9d1b2a63e576b953e9e0c063285deeb4
ian <iangurgel25@hotmail.com>
2025-02-13 18:46:23 -0300

    terminado integração da navbar com busca de filmes em novo componente

---
+++ client-side/src/components/SearchMovie.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useQuery } from 'react-query';
+import MovieCard from './MovieCard';
+
+function SearchMovie({ movieName }) {
+    const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+    const BASE_URL = "https://api.themoviedb.org/3/"
+
+    const { data, isLoading, error } = useQuery(['movie_name', movieName], () =>
+        fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${movieName}`)
+            .then((res) => res.json())
+    );
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Erro: {error.message}</div>;
+
+    return (
+        <div>
+            <div className='movie-grid'>
+                {data?.results.map((movie) => (
+                    <div key={movie.id} className='flex flex-col items-center '>
+                        <MovieCard movie={movie} />
+                        
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default SearchMovie
--- client-side/src/pages/Home.jsx
+++ client-side/src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import FetchMovies from '../components/FetchMovies'
+import SearchMovie from '../components/SearchMovie'
+import { useState } from 'react'
 
 function Home() {
+  const [SearchMovieName, setSearchMovieName] = useState('');
+
+  const handleSearch = (movieName) => {
+    setSearchMovieName(movieName); 
+  };
+
   return (
     <div>
-        <Navbar />
-        <FetchMovies />
+        <Navbar onSearch={handleSearch}/>
+        { SearchMovieName ? <SearchMovie movieName={SearchMovieName} /> : <FetchMovies /> }
     </div>
   )
 }
