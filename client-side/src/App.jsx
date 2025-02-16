70fe8ab3c2ade8ce01c26629c67fc207e2d1883c
ian <iangurgel25@hotmail.com>
2025-02-16 18:31:14 -0300

    update na navbar

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import ErrorPage from './pages/ErrorPage';
 
+
+
 // Cria uma instância do QueryClient
 const queryClient = new QueryClient();
 
--- client-side/src/pages/Login.jsx
+++ client-side/src/pages/Login.jsx
@@ -10,13 +10,12 @@ function Login() {
 
   return (
     <div className=''>
-      <Navbar></Navbar>
+      <Navbar />
       <div className='mx-auto bg-slate-800 border border-blue-400 w-[400px] md:w-[600px] flex flex-col rounded-xl px-4 mt-10'>
         <p className='text-blue-400 font-semibold my-5 text-2xl'>{isLoginMenu ? "Log in your account" : "Create your account"}</p>
 
         <div className="flex flex-col gap-y-3">
           <input type="text" className='bg-slate-300 rounded-sm outline-none pl-2 py-1' placeholder='Username' />
-          <input type="text" className='bg-slate-300 rounded-sm outline-none pl-2 py-1' placeholder='Email' />
           <div className="flex items-center bg-slate-300 rounded-sm">
             <input type={isPasswordVisible ? "text" : "password"} className=' flex grow outline-none pl-2 py-1' placeholder='Password' />
             <i className={`fa-solid  ${isPasswordVisible ? "fa-eye" : "fa-eye-slash"} cursor-pointer px-2 py-1`}
